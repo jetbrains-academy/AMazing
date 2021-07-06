119286b696497540b022b7b4f2d5235ebb20f9f2
sofiiako <19rubisco93@gmail.com>
2021-07-06 18:21:20 +0300

    Added a print statement to Create a Cell to print some output

--- lesson1/Create a Cell/task.py
+++ lesson1/Create a Cell/task.py
@@ -2,3 +2,10 @@ class Cell:
 
     def __init__(self, x, y):
         self.x, self.y = x, y
+
+
+if __name__ == '__main__':
+    cell = Cell(0, 0)
+    print(f'Cell\n'
+          f'coordinate x: {cell.x}\n'
+          f'coordinate y: {cell.y}')
