08e85c5f51fd210230d641f905d47df4439a4374
sofiiako <19rubisco93@gmail.com>
2021-07-07 17:59:01 +0300

    Updated output code to draw grid in Knock Down Wall

--- lesson1/Knock Down Wall/output.py
+++ lesson1/Knock Down Wall/output.py
@@ -2,10 +2,10 @@ from task import Cell
 from PIL import Image, ImageDraw
 
 
-def draw_cell(cell, image):
+def draw_cell(cell, image, color="black"):
     filt = [x for x in cell.walls.values()]
-    x = 90 if cell.x == 0 else 190
-    y = 90 if cell.y == 0 else 190
+    x = 90 + cell.x * 100
+    y = 90 + cell.y * 100
 
     north, south, east, west = [(x - 50, y - 50), (x + 50, y - 50)], \
                                [(x - 50, y + 50), (x + 50, y + 50)], \
@@ -14,11 +14,26 @@ def draw_cell(cell, image):
     lines = north, south, east, west
     filtered_lines = [i for (i, v) in zip(lines, filt) if v]
     for line in filtered_lines:
-        image.line(line, fill="black", width=5)
+        image.line(line, fill=color, width=5)
+
+
+def draw_grid(image, x_cells, y_cells):
+    cels = generate_cells(x_cells, y_cells)
+    for cel in cels.values():
+        draw_cell(cel, image, "lightgray")
+
+
+def get_dimensions(*cells):
+    xs = [cell.x for cell in cells]
+    ys = [cell.y for cell in cells]
+    return max(xs) + 1, max(ys) + 1
 
 
 def draw_image(image, filename, *cells):
     img1 = ImageDraw.Draw(image)
+    xc = get_dimensions(*cells)[0]
+    yc = get_dimensions(*cells)[1]
+    draw_grid(img1, xc, yc)
     for cell in cells:
         draw_cell(cell, img1)
 
@@ -26,21 +41,34 @@ def draw_image(image, filename, *cells):
     image.save(filename)
 
 
+def generate_cells(x, y):
+    cells_ = {}
+    count = 0
+    for i in range(x):
+        for j in range(y):
+            cell_name = "Cell%d" % count
+            count += 1
+            cells_[cell_name] = Cell(i, j)
+    return cells_
+
+
 if __name__ == '__main__':
-    cell1 = Cell(0, 0)
-    cell2 = Cell(1, 0)
+    # dim1 = int(input())
+    # dim2 = int(input())
+    dim1, dim2 = 1, 2
+    cells = generate_cells(dim1, dim2)
 
-    w, h = 280, 180
+    w, h = (80 + 100 * dim for dim in get_dimensions(*cells.values()))
 
-    # draw an image of two adjacent cells before knocking down the wall
+    # Draw an image of two adjacent cells before knocking down the wall
     img = Image.new("RGB", (w, h), (255, 255, 255))
-    draw_image(img, "adjacent_cells.png", cell1, cell2)
+    draw_image(img, "adjacent_cells.png", *cells.values())
 
-    # Knock down an E wall of the (0,0) cell
-    cell1.knock_down_wall(cell2, 'E')
-    print(f'Cell 1 walls: {cell1.walls}')
-    print(f'Cell 2 walls: {cell2.walls}')
+    # Knock down an S wall of the (0,0) cell
+    cells["Cell0"].knock_down_wall(cells["Cell1"], 'S')
+    print(f'Cell 0 walls: {cells["Cell0"].walls}')
+    print(f'Cell 1 walls: {cells["Cell1"].walls}')
 
-    # draw an image of two adjacent cells after knocking down the wall
+    # Draw an image of two adjacent cells after knocking down the wall
     img2 = Image.new("RGB", (w, h), (255, 255, 255))
-    draw_image(img2, "removed_E_wall.png", cell1, cell2)
+    draw_image(img2, "removed_NS_wall.png", *cells.values())
