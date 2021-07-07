d7d86524ecf71ae84d6d1d2ea11ad4f107a3934e
sofiiako <19rubisco93@gmail.com>
2021-07-07 13:01:36 +0300

    Added a drawing of the cell with its walls to Add Walls to the Cell

--- lesson1/Add Walls to the Cell/task.py
+++ lesson1/Add Walls to the Cell/task.py
@@ -14,19 +14,20 @@ if __name__ == '__main__':
 
     # setting up dimensions and line coordinates
     w, h = 200, 200
-    shape1, shape2, shape3, shape4 = [(40, 40), (w - 40, 40)], \
-                                     [(40, 40), (40, h - 40)], \
-                                     [(w - 40, 40), (w - 40, h - 40)], \
-                                     [(40, h - 40), (w - 40, h - 40)]
-    shapes = shape1, shape2, shape3, shape4
-    filtered_shapes = [i for (i, v) in zip(shapes, filter_) if v]
+    line1, line2, line3, line4 = [(40, 40), (w - 40, 40)], \
+                                 [(40, h - 40), (w - 40, h - 40)],\
+                                 [(40, 40), (40, h - 40)],\
+                                 [(w - 40, 40), (w - 40, h - 40)]
+
+    lines = line1, line2, line3, line4
+    filtered_lines = [i for (i, v) in zip(lines, filter_) if v]
 
     # creating new Image object
     img = Image.new("RGB", (w, h), (255, 255, 255))
 
     # create line image
     img1 = ImageDraw.Draw(img)
-    for shape in filtered_shapes:
-        img1.line(shape, fill="black", width=5)
+    for line in filtered_lines:
+        img1.line(line, fill="black", width=5)
     img.show()
     img.save("cell_walls.png")
