177a50ecfc4152f97ac6ab9b733d96c984b70b56
sofiiako <19rubisco93@gmail.com>
2021-07-07 13:18:26 +0300

    Moved output code to separate file in Create a Cell

---
+++ lesson1/Create a Cell/output.py
@@ -0,0 +1,8 @@
+from task import Cell
+
+
+if __name__ == '__main__':
+    cell = Cell(0, 0)
+    print(f'Cell\n'
+          f'coordinate x: {cell.x}\n'
+          f'coordinate y: {cell.y}')
--- lesson1/Create a Cell/task.py
+++ lesson1/Create a Cell/task.py
@@ -2,10 +2,3 @@ class Cell:
 
     def __init__(self, x, y):
         self.x, self.y = x, y
-
-
-if __name__ == '__main__':
-    cell = Cell(0, 0)
-    print(f'Cell\n'
-          f'coordinate x: {cell.x}\n'
-          f'coordinate y: {cell.y}')
