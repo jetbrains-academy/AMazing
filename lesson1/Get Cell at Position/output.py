18fce956e7e59048e888bb7c81956612f8d6f23b
sofiiako <19rubisco93@gmail.com>
2021-07-14 17:54:28 +0300

    Removed unneeded code in Get Cell at Position

--- lesson1/Get Cell at Position/output.py
+++ lesson1/Get Cell at Position/output.py
@@ -30,27 +30,12 @@ def draw_grid(image, x_cells, y_cells):
         draw_cell(cel, image, "lightgray")
 
 
-def get_dimensions(*cells):
-    return max([cell.x for cell in cells]) + 1, max([cell.y for cell in cells]) + 1
-
-
-def generate_cells(x, y):
-    cells_ = {}
-    count = 0
-    for i in range(x):
-        for j in range(y):
-            cell_name = "Cell%d" % count
-            count += 1
-            cells_[cell_name] = Cell(i, j)
-    return cells_
-
-
 if __name__ == '__main__':
     dim1 = int(input('Enter x dimension: '))
     dim2 = int(input('Enter y dimension: '))
     maze = Maze(dim1, dim2)
 
-    # Print the cell selected using the method defined in the task
+    # Print the cell selected using the method defined in the task:
     print(maze.cell_at(dim1 - 1, dim2 - 1))
 
     w, h = (80 + 100 * dim for dim in maze.maze_grid.shape)
@@ -58,9 +43,8 @@ if __name__ == '__main__':
     img1 = ImageDraw.Draw(img)
     draw_grid(img1, maze.maze_grid.shape[0], maze.maze_grid.shape[1])
 
-    # Highlight the cell selected using the method defined in the task
+    # Highlight a selected cell using the method defined in the task:
     highlighted_cell = maze.cell_at(int(input('Enter x coordinate of cell to highlight: ')),
                                     int(input('Enter y coordinate of cell to highlight: ')))
     draw_cell(highlighted_cell, img1)
-
     img.save('highlighted_cell.png')
