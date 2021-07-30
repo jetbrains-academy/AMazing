a84abf6a5b850f965cc89e60ab9e01b313b9b5e4
sofiiako <19rubisco93@gmail.com>
2021-07-30 13:25:59 +0300

    refactor(Create a Cell output.py): Changed output string

--- lesson1/Create a Cell/output.py
+++ lesson1/Create a Cell/output.py
@@ -4,5 +4,5 @@ from task import Cell
 if __name__ == '__main__':
     cell = Cell(0, 0)
     print(f'Cell\n'
-          f'coordinate x: {cell.x}\n'
-          f'coordinate y: {cell.y}')
+          f'x coordinate: {cell.x}\n'
+          f'y coordinate: {cell.y}')
