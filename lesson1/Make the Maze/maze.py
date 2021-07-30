6c6136c73c6958d21aac19ef90cb366382d78d78
sofiiako <19rubisco93@gmail.com>
2021-07-30 19:32:52 +0300

    commented the svg visualization

--- lesson1/Make the Maze/maze.py
+++ lesson1/Make the Maze/maze.py
@@ -26,36 +26,36 @@ class Maze:
                     neighbours.append((direction, neighbour))
         return neighbours
 
-    def write_svg(self, filename):
-        """Write an SVG image of the maze to filename."""
-        scy, scx = 500 / self.ny, 500 / self.nx
-
-        def write_wall(ww_f, ww_x1, ww_y1, ww_x2, ww_y2):
-            ww_f.write(f'<line x1="{ww_x1}" y1="{ww_y1}" x2="{ww_x2}" y2="{ww_y2}"/>')
-
-        with open(filename, 'w') as f:
-            f.write('<?xml version="1.0" encoding="utf-8"?>')
-            f.write('<svg xmlns="http://www.w3.org/2000/svg"')
-            f.write('    xmlns:xlink="http://www.w3.org/1999/xlink"')
-            f.write('    width="520" height="520" viewBox="-10 -10 520 520">')
-            f.write('<defs>\n<style type="text/css"><![CDATA[')
-            f.write('line {')
-            f.write('    stroke: #000000;\n    stroke-linecap: square;')
-            f.write('    stroke-width: 5;\n}')
-            f.write(']]></style>\n</defs>')
-
-            for x in range(self.nx):
-                for y in range(self.ny):
-                    if self.cell_at(x, y).walls['S']:
-                        x1, y1, x2, y2 = x * scx, (y + 1) * scy, (x + 1) * scx, (y + 1) * scy
-                        write_wall(f, x1, y1, x2, y2)
-                    if self.cell_at(x, y).walls['E']:
-                        x1, y1, x2, y2 = (x + 1) * scx, y * scy, (x + 1) * scx, (y + 1) * scy
-                        write_wall(f, x1, y1, x2, y2)
-
-            f.write('<line x1="0" y1="0" x2="500" y2="0"/>')
-            f.write('<line x1="0" y1="0" x2="0" y2="500"/>')
-            f.write('</svg>')
+    # def write_svg(self, filename):
+    #     """Write an SVG image of the maze to filename."""
+    #     scy, scx = 500 / self.ny, 500 / self.nx
+    #
+    #     def write_wall(ww_f, ww_x1, ww_y1, ww_x2, ww_y2):
+    #         ww_f.write(f'<line x1="{ww_x1}" y1="{ww_y1}" x2="{ww_x2}" y2="{ww_y2}"/>')
+    #
+    #     with open(filename, 'w') as f:
+    #         f.write('<?xml version="1.0" encoding="utf-8"?>')
+    #         f.write('<svg xmlns="http://www.w3.org/2000/svg"')
+    #         f.write('    xmlns:xlink="http://www.w3.org/1999/xlink"')
+    #         f.write('    width="520" height="520" viewBox="-10 -10 520 520">')
+    #         f.write('<defs>\n<style type="text/css"><![CDATA[')
+    #         f.write('line {')
+    #         f.write('    stroke: #000000;\n    stroke-linecap: square;')
+    #         f.write('    stroke-width: 5;\n}')
+    #         f.write(']]></style>\n</defs>')
+    #
+    #         for x in range(self.nx):
+    #             for y in range(self.ny):
+    #                 if self.cell_at(x, y).walls['S']:
+    #                     x1, y1, x2, y2 = x * scx, (y + 1) * scy, (x + 1) * scx, (y + 1) * scy
+    #                     write_wall(f, x1, y1, x2, y2)
+    #                 if self.cell_at(x, y).walls['E']:
+    #                     x1, y1, x2, y2 = (x + 1) * scx, y * scy, (x + 1) * scx, (y + 1) * scy
+    #                     write_wall(f, x1, y1, x2, y2)
+    #
+    #         f.write('<line x1="0" y1="0" x2="500" y2="0"/>')
+    #         f.write('<line x1="0" y1="0" x2="0" y2="500"/>')
+    #         f.write('</svg>')
 
     def make_maze(self):
         n = self.nx * self.ny
