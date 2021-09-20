d1049635095554e34a501babbdd2cac6b4552c36
sofiiako <19rubisco93@gmail.com>
2021-09-20 19:03:00 +0300

    Added a message specifying what could be wrong with the maze algorithm and how to fix it.

--- lesson1/Make the Maze/tests/test_task.py
+++ lesson1/Make the Maze/tests/test_task.py
@@ -75,4 +75,8 @@ class TestCase(unittest.TestCase):
         maze_ = Maze(5, 5)
         maze_.make_maze()
         test_ = [True if test_reachable(maze_) else False for i in range(100)]
-        self.assertTrue(all(test_), msg='Some cells appear unreachable in your maze :(')
+        self.assertTrue(all(test_), msg='Some cells appear unreachable in your maze :('
+                                        'This could happen if not all of the grid cells were visited '
+                                        'by your algorithm, or if walls were not knocked down.'
+                                        'Make sure both of these things happen. Visualization could'
+                                        'also be helpful.')
